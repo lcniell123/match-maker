8a11839a5cac73f4cc39d93cc3e8bb7756bd4f19
lcn <lcniell@gmail.com>
2024-03-23 19:38:33 -0700

    update

--- src/app/components/todos/todos.tsx
+++ src/app/components/todos/todos.tsx
@@ -1,95 +1,89 @@
-'use client';
+"use client";
 
-import { useRouter } from "next/navigation";
+// import { useRouter } from "next/navigation";
 
+// // import * as mutations from '@/graphql/mutations';
+// // 1. Add the queries as an import
+// // import * as queries from '@/graphql/queries';
 
-import * as mutations from '@/graphql/mutations';
-// 1. Add the queries as an import
-import * as queries from '@/graphql/queries';
+// import { generateClient } from 'aws-amplify/api';
+// import { useEffect, useState } from 'react';
+// const client = generateClient();
 
-import { generateClient } from 'aws-amplify/api';
-import { useEffect, useState } from 'react';
-const client = generateClient();
+// async function createTodo(formData: FormData) {
 
+//   const { data } = await client.graphql({
+//     query: mutations.createTodo,
+//     variables: {
+//       input: {
+//         name: formData.get('name')?.toString() ?? ''
+//       }
+//     }
+//   });
 
-async function createTodo(formData: FormData) {
+// }
 
+// export default  function Todos() {
+//   const router = useRouter()
 
-  const { data } = await client.graphql({
-    query: mutations.createTodo,
-    variables: {
-      input: {
-        name: formData.get('name')?.toString() ?? ''
-      }
-    }
-  });
+//   const [allTodos,setAllTodos]= useState<any>([])
+//   const [newTodos, setNewTodos]=useState<any>([])
 
+//   // 2. Fetch additional todos
+//   // const { data, errors } = await client.graphql({
+//   //   query: queries.listTodos
+//   // });
 
-}
+//   // console.log("data",data.data)
+//   const  data = client.graphql({
+//     query: queries.listTodos
+//   });
+//   useEffect(()=>{
 
-export default  function Todos() {
-  const router = useRouter()
-  
-  const [allTodos,setAllTodos]= useState<any>([])
-  const [newTodos, setNewTodos]=useState<any>([])
+//     data.then((d)=>{
+//       console.log(d.data.listTodos.items)
+// setAllTodos(d.data.listTodos.items)
 
-  // 2. Fetch additional todos
-  // const { data, errors } = await client.graphql({
-  //   query: queries.listTodos
-  // });
+//     })
+//   },[])
 
-   
-  // console.log("data",data.data)
-  const  data = client.graphql({
-    query: queries.listTodos
-  });
-  useEffect(()=>{
-    
-    data.then((d)=>{
-      console.log(d.data.listTodos.items)
-setAllTodos(d.data.listTodos.items)
+//   useEffect(()=>{
+//    setNewTodos(allTodos)
+//   },[allTodos])
 
-    })
-  },[])
+//   const handleRefresh = () => {
+//     router.push('/')
+//     router.refresh();
+//   };
 
-  useEffect(()=>{
-   setNewTodos(allTodos)
-  },[allTodos])
+//   return (
+//     <div
+//       style={{
+//         maxWidth: '500px',
+//         margin: '0 auto',
+//         textAlign: 'center',
+//         marginTop: '100px'
+//       }}
+//     >
+//       <form action={createTodo}>
+//         <input name="name" placeholder="Add a todo" />
+//         <button type="submit" onClick={handleRefresh} >Add</button>
+//       </form>
 
-  const handleRefresh = () => {
-    router.push('/')
-    router.refresh();
-  };
+//       {/* 3. Handle edge cases & zero state & error states*/}
+//       {(!newTodos || newTodos.length === 0 ) && (
+//         <div>
+//           <p>No todos, please add one.</p>
+//         </div>
+//       )}
 
+//       {/* 4. Display todos*/}
+//       <ul>
+//         {newTodos.map((todo:any) => {
+//           return <li style={{ listStyle: 'none' }}>{todo.name}</li>;
+//         })}
+//       </ul>
+//     </div>
 
-  return (
-    <div
-      style={{
-        maxWidth: '500px',
-        margin: '0 auto',
-        textAlign: 'center',
-        marginTop: '100px'
-      }}
-    >
-      <form action={createTodo}>
-        <input name="name" placeholder="Add a todo" />
-        <button type="submit" onClick={handleRefresh} >Add</button>
-      </form>
-
-      {/* 3. Handle edge cases & zero state & error states*/}
-      {(!newTodos || newTodos.length === 0 ) && (
-        <div>
-          <p>No todos, please add one.</p>
-        </div>
-      )}
-
-      {/* 4. Display todos*/}
-      <ul>
-        {newTodos.map((todo:any) => {
-          return <li style={{ listStyle: 'none' }}>{todo.name}</li>;
-        })}
-      </ul>
-    </div>
-    
-  );
-}
+//   );
+// }
