05846f527635303081b14b029278058500c9b977
lcn <lcniell@gmail.com>
2024-03-23 19:47:37 -0700

    remove todo auth

--- src/app/components/auth.tsx
+++ src/app/components/auth.tsx
@@ -1,25 +1,24 @@
+import React from "react";
+import { Amplify } from "aws-amplify";
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
 
-
-import React from 'react';
-import { Amplify } from 'aws-amplify';
-import { Authenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
-
-import awsExports from '../../amplifyconfiguration.json';
- import Todos from './todos/todos';
+import awsExports from "../../amplifyconfiguration.json";
+//simport Todos from './todos/todos';
 Amplify.configure(awsExports);
 
 export default function Auth() {
   return (
-    <Todos/>
-    // <Authenticator>
-    //   {({ signOut, user }) => (
-    //     <main>
-    //       <h1>Hello {user?.username}</h1>
-        
-    //       <button onClick={signOut}>Sign out</button>
-    //     </main>
-    //   )}
-    // </Authenticator>
+    <></>
+    //   <Todos/>
+    //   // <Authenticator>
+    //   //   {({ signOut, user }) => (
+    //   //     <main>
+    //   //       <h1>Hello {user?.username}</h1>
+
+    //   //       <button onClick={signOut}>Sign out</button>
+    //   //     </main>
+    //   //   )}
+    //   // </Authenticator>
   );
 }
