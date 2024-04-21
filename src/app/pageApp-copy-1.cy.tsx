51774f7135f865ceefad51fe23cbddd60f66da56
Pollypanda <ploypawachot33@gmail.com>
2024-04-20 23:27:45 -0400

    Update pageApp-copy-1.cy.tsx

--- src/app/pageApp-copy-1.cy.tsx
+++ src/app/pageApp-copy-1.cy.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import App from './page'
+// import React from 'react'
+// import App from './page'
 
-xdescribe('<App />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<App />)
-  })
-})
+// xdescribe('<App />', () => {
+//   it('renders', () => {
+//     // see: https://on.cypress.io/mounting-react
+//     cy.mount(<App />)
+//   })
+// })
