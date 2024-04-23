580cb274282e14df95400b02926b5205f1c357ff
lcn <lcniell@gmail.com>
2024-04-23 10:37:17 -0700

    update friend query

--- src/app/tabs/Friends.tsx
+++ src/app/tabs/Friends.tsx
@@ -1,56 +1,58 @@
-import React, { useState, useEffect } from 'react';
-import {getCurrentUser} from "aws-amplify/auth";
-import {generateClient} from "aws-amplify/api";
+import React, { useState, useEffect } from "react";
+import { getCurrentUser } from "aws-amplify/auth";
+import { generateClient } from "aws-amplify/api";
 import * as queries from "@/graphql/queries";
-import {Friendships} from "@/API";
+import { Friendships, RelationshipStatus } from "@/API";
 
+export default function Friends() {
+  const [userId, setUserId] = useState("");
+  const [friends, setFriends] = useState<Friendships[]>([]);
+  const client = generateClient();
 
-export default function Friends()  {
-    const [userId, setUserId] = useState("");
-    const [friends, setFriends] = useState<Friendships[]>([]);
-    const client = generateClient();
+  useEffect(() => {
+    async function currentAuthenticatedUser() {
+      try {
+        const { userId } = await getCurrentUser();
+        setUserId(userId);
+      } catch (err) {
+        console.log(err);
+      }
+    }
 
-    useEffect(() => {
-        async function currentAuthenticatedUser() {
-            try {
-                const { userId } = await getCurrentUser();
-                setUserId(userId);
-            } catch (err) {
-                console.log(err);
-            }
-        }
+    currentAuthenticatedUser();
+  }, []);
 
-        currentAuthenticatedUser();
-    }, []);
+  useEffect(() => {
+    async function fetchFriends() {
+      try {
+        const client = generateClient();
+        const response = await client.graphql({
+          query: queries.listFriendships,
+          variables: {
+            filter: {
+              id: { eq: userId },
+              friendshipStatus: { eq: RelationshipStatus.FRIEND },
+            },
+          },
+        });
+        setFriends(response.data.listFriendships.items);
+      } catch (error) {
+        console.error("Error fetching friends:", error);
+      }
+    }
 
-    useEffect(() => {
-        async function fetchFriends() {
-            try {
-                const client = generateClient();
-                const response = await client.graphql({
-                    query: queries.listFriendships,
-                    variables: {
-                        filter: { userId: { eq: userId },
-                                  status: { eq: "accepted" } } } })
-                setFriends(response.data.listFriendships.items);
-            } catch (error) {
-                console.error("Error fetching friends:", error);
-            }
-        }
+    if (userId) {
+      fetchFriends();
+    }
+  }, [userId]);
 
-        if (userId) {
-            fetchFriends();
-        }
-    }, [userId]);
-
-    return (
-        <div>
-            <ul>
-                {friends.map(friend => (
-                    <li key={friend.id}>{friend.friendId}</li>
-                ))}
-            </ul>
-        </div>
-    );
+  return (
+    <div>
+      <ul>
+        {friends.map((friend) => (
+          <li key={friend.id}>{friend.id}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
-
