a0aa8c14409f8a45d26d79802e725ed42fed42de
lcn <lcniell@gmail.com>
2024-04-24 11:53:02 -0700

    navigation test xit

--- src/app/components/MenuNavigationNavigation.cy.tsx
+++ src/app/components/MenuNavigationNavigation.cy.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import { Navigation } from './MenuNavigation'
+import React from "react";
+import { Navigation } from "./MenuNavigation";
 
-describe('<Navigation />', () => {
-  it('should allow users to click on notification button to check their friend requests', () => {
+describe("<Navigation />", () => {
+  xit("should allow users to click on notification button to check their friend requests", () => {
     // see: https://on.cypress.io/mounting-react
-    cy.mount(<Navigation />)
+    cy.mount(<Navigation />);
     // cy.get('.p-1 > .sr-only')
-      // Click the button to open the user menu
-      cy.get('.p-1 > .sr-only').click();
-    });
-})
+    // Click the button to open the user menu
+    cy.get(".p-1 > .sr-only").click();
+  });
+});
