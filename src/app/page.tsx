a9cd1801cbd17a5d753a170447a462a38a9dea2a
lcn <lcniell@gmail.com>
2024-05-07 08:58:46 -0700

    update url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { Amplify } from "aws-amplify";
 import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
-import awsExports from "../../amplifyconfiguration.json";
+import awsExports from "../amplifyconfiguration.json";
 import { useRouter } from "next/navigation";
 import Dashboard from "@/app/components/Dashboard";
 
